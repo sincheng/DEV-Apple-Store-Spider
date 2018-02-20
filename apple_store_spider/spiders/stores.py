2ccda5b0922a36fd324a14f7900ec75af077ae31
Sin Cheng <scheng6@stevens.edu>
2018-02-19 21:49:40 -0500

    Simplify the code

--- apple_store_spider/spiders/stores.py
+++ apple_store_spider/spiders/stores.py
@@ -1,69 +1,46 @@
 import scrapy
 from apple_store_spider.geocode import getCoordinates
-import httplib2
 
-def find_address(response):
-    address = response.xpath('//*[@id="gallery-mapSwap-section-2"]/div[1]/div[1]/span/text()').extract()
+#Function to return decoded full address
+def find_address(address_response):
+    region= address_response.xpath('.//*[contains(@class,"region")]/text()').extract_first()
+    city = address_response.xpath('.//*[contains(@class,"locality")]/text()').extract_first()
+    address = address_response.xpath('.//span/text()').extract()
     #Remove phone number
     address = address[:-1]
-    decoded_address = list(map(lambda x:x.encode('utf-8').decode('utf-8'),address))
-    return ", ".join(decoded_address)
-
-    #//*[@id="main"]/section[3]/div/div/div[3]/div/div/p[1]
-    #//*[@id="main"]/section[3]/div/div/div[3]/div/div/p[1]
-
+    return region or "NA", city or "NA", ", ".join(address).encode('utf-8').decode('utf-8')
 
 class StoresSpider(scrapy.Spider):
     name = 'stores'
     allowed_domains = ['apple.com']
-    start_urls = ['http://apple.com/retail/storelist//']
-
-
+    start_urls = ['http://apple.com/retail/storelist/']
     def parse(self, response):
         apple_url = 'https://www.apple.com'
-        #List of countries
+        #List of countries and countries id
         countries_id = response.xpath('//@data-tag').extract()
         countries = response.xpath('//*[@data-tag]/text()').extract()
-        id_stores = list(map(lambda s: s + "stores", countries_id))
+        id_list = [country_id + "stores" for country_id in countries_id]
 
-        for s_id, country in zip(id_stores, countries):
-        #
-            store = response.xpath('//*[@id="'+s_id+'"]')
-        #     states = store.xpath('.//*[@class="toggle-section"]')
-        #store = response.xpath('//*[@id="usstores"]')
-        #states_response = store.xpath('.//*[@class="toggle"]')
-            states_response = store.xpath('.//*[@class="toggle-section"]')
-            states_response = ["NA"] if states_response ==[] else states_response
-
-
-            for state in states_response:
-                if state=="NA":
-                    state_name = state
-                    cities_temp = store.xpath('./div/div/div//ul/li[1]/text()').extract()
-                    url_list = store.xpath('./div/div/div//ul/li/a/@href').extract()
-                else:
-                    state_name =  state if state == "NA" else state.xpath('./h3/text()').extract_first()
-                    cities_temp = state.xpath('.//li/text()').extract()
-                    url_list = state.xpath('.//li/a/@href').extract()
-                cities = list(map(lambda x:x.replace(", ",""),cities_temp))
-                for city , url in zip(cities, url_list):
-                    store_url = apple_url + url
-                    yield scrapy.Request(store_url,callback=self.parse_store , meta={'country':country,'state': state_name , 'city':city})
+        for s_id, country in zip(id_list, countries):
+            url_list = response.xpath('//*[@id="'+s_id+'"]//ul/li/a/@href').extract()
+            for store in url_list:
+                store_url = apple_url + store
+                yield scrapy.Request(store_url,callback=self.parse_store , meta={'country':country})
 
     def parse_store(self,response):
         store_name = response.xpath('//h1/text()').extract_first()
+        address_response = response.xpath('//*[@id="gallery-mapSwap-section-1"]/div[2]/div[1]')
+        region, city, store_address = find_address(address_response)
         img_url = response.urljoin("images/hero_medium.jpg")
-        store_address = find_address(response)
-        latitude = getCoordinates(store_address)[0]
-        longitude = getCoordinates(store_address)[1]
-
+        coordinates = getCoordinates(store_address)
+        latitude , longitude = coordinates[0] , coordinates[1]
         yield {
             "Country":response.meta['country'],
-            "Region":response.meta['state'],
-            "City":response.meta['city'],
-            "Store_name":store_name.encode('utf-8').decode('utf-8'),
-            "Img_url":img_url,
-            "Store_address":store_address,
+            "Region":region,
+            "City":city,
+            "Name":store_name.encode('utf-8').decode('utf-8'),
+            "Img":img_url,
+            "Address":store_address,
             "Lat":latitude,
             "Long":longitude
             }
